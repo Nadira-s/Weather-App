bdbce72d0c5774ae3f961c27d3cb83b6fec4af71
Nadira Seitkazy <nadiraseitkazu@gmail.com>
2026-01-28 13:33:47 +0500

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Nadira Seitkazy  on 28.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
